e2b1ef1ba929770b0a1d60e6cd3fa6fc42a9c770
Rafael Jordani Coelho <rafitapc08@gmail.com>
2026-08-06 19:13:30 -0300

    feture/tela escrolavel quando estiver com a barra de digitar aberta

--- lib/pages/signup_page.dart
+++ lib/pages/signup_page.dart
@@ -1,4 +1,3 @@
-import 'package:ecommerce/shared/app_colors.dart';
 import 'package:ecommerce/shared/app_text_style.dart';
 import 'package:ecommerce/shared/widgets/app_check_box.dart';
 import 'package:ecommerce/shared/widgets/app_elevated_button.dart';
@@ -39,102 +38,112 @@ class _SignupPageState extends State<SignupPage> {
     print(ModalRoute.of(context)!.settings.arguments);
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: Column(
-            spacing: 15,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Align(child: Text('Criar uma conta', style: AppTextStyle.title)),
-              Align(
-                child: Text(
-                  'Insira seus dados para iniciar suas compras',
-                  style: AppTextStyle.subTitle,
-                ),
-              ),
-              AppTextField(
-                hintText: 'email@dominio.com',
-                onChanged: (value) {
-                  setState(() {
-                    email = value;
-                  });
-                },
-              ),
-              AppTextField(
-                hintText: 'nome',
-                onChanged: (value) {
-                  setState(() {
-                    nome = value;
-                  });
-                },
-              ),
-              AppTextField(
-                hintText: 'senha',
-                obscureText: true,
-                onChanged: (value) {
-                  setState(() {
-                    senha = value;
-                  });
-                },
-              ),
-              AppTextField(
-                hintText: 'confirmar senha ',
-                obscureText: true,
-                onChanged: (value) {
-                  setState(() {
-                    confirmarSenha = value;
-                  });
-                },
-              ),
-              const Spacer(flex: 2),
-
-              Row(
+        child: SingleChildScrollView(
+          child: SizedBox(
+            height:
+                MediaQuery.of(context).size.height -
+                MediaQuery.of(context).padding.top -
+                MediaQuery.of(context).padding.bottom,
+            child: Padding(
+              padding: const EdgeInsets.all(24.0),
+              child: Column(
+                spacing: 15,
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: [
-                  AppCheckbox(
-                    isActiveChecked,
+                  Align(
+                    child: Text('Criar uma conta', style: AppTextStyle.title),
+                  ),
+                  Align(
+                    child: Text(
+                      'Insira seus dados para iniciar suas compras',
+                      style: AppTextStyle.subTitle,
+                    ),
+                  ),
+                  AppTextField(
+                    hintText: 'email@dominio.com',
                     onChanged: (value) {
                       setState(() {
-                        isActiveChecked = !isActiveChecked;
+                        email = value;
                       });
                     },
                   ),
-                  RichText(
-                    textAlign: TextAlign.center,
-                    text: TextSpan(
-                      children: [
-                        TextSpan(
-                          text:
-                              'Ao clicar em continuar, você concorda com os nossos \n',
-                          style: AppTextStyle.texSpanGrey,
+                  AppTextField(
+                    hintText: 'nome',
+                    onChanged: (value) {
+                      setState(() {
+                        nome = value;
+                      });
+                    },
+                  ),
+                  AppTextField(
+                    hintText: 'senha',
+                    obscureText: true,
+                    onChanged: (value) {
+                      setState(() {
+                        senha = value;
+                      });
+                    },
+                  ),
+                  AppTextField(
+                    hintText: 'confirmar senha ',
+                    obscureText: true,
+                    onChanged: (value) {
+                      setState(() {
+                        confirmarSenha = value;
+                      });
+                    },
+                  ),
+                  const Spacer(flex: 2),
+
+                  Row(
+                    children: [
+                      AppCheckbox(
+                        isActiveChecked,
+                        onChanged: (value) {
+                          setState(() {
+                            isActiveChecked = !isActiveChecked;
+                          });
+                        },
+                      ),
+                      RichText(
+                        textAlign: TextAlign.center,
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text:
+                                  'Ao clicar em continuar, você concorda com os nossos \n',
+                              style: AppTextStyle.texSpanGrey,
+                            ),
+                            TextSpan(
+                              text: 'Termos de Serviço',
+                              style: AppTextStyle.texSpanBlack,
+                            ),
+                            TextSpan(
+                              text: 'e com a ',
+                              style: AppTextStyle.texSpanGrey,
+                            ),
+                            TextSpan(
+                              text: 'Política de Privacidade',
+                              style: AppTextStyle.texSpanBlack,
+                            ),
+                          ],
                         ),
-                        TextSpan(
-                          text: 'Termos de Serviço',
-                          style: AppTextStyle.texSpanBlack,
-                        ),
-                        TextSpan(
-                          text: 'e com a ',
-                          style: AppTextStyle.texSpanGrey,
-                        ),
-                        TextSpan(
-                          text: 'Política de Privacidade',
-                          style: AppTextStyle.texSpanBlack,
-                        ),
-                      ],
-                    ),
+                      ),
+                    ],
+                  ),
+                  AppElevatedButton(
+                    type: ButtonType.filled,
+                    textButton: 'Continuar ',
+                    onPressed: isActiveButton
+                        ? () {
+                            print('cliquei em continuar');
+                          }
+                        : null,
                   ),
                 ],
               ),
-              AppElevatedButton(
-                type: ButtonType.filled,
-                textButton: 'Continuar ',
-                onPressed: isActiveButton
-                    ? () {
-                        print('cliquei em continuar');
-                      }
-                    : null,
-              ),
-            ],
+            ),
           ),
         ),
       ),
